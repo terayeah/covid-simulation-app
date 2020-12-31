01317dc747d2d1a5a2a458f9213c5e87c4dff89a
dterai <dterai@student.42tokyo.jp>
2020-12-31 15:13:01 +0900

    枠組みの作成

--- js/sketch.js
+++ js/sketch.js
@@ -1,22 +1,26 @@
 let people = [];
-let r = 22;
+let r = 20;
 let maxDensity;
+let field;
+let infoField;
 
 function setup() {
-    maxDensity = windowWidth/(r*2) * windowHeight/(r*2)
+    infoField = {w: windowWidth, h:200}
+    field = {w: windowWidth, h:windowHeight-infoField.h, offset:10}
+    maxDensity = field.w/(r*2) * field.h/(r*2)
     createCanvas(windowWidth, windowHeight);
     for(var i = 0; i < maxDensity; i++){
-        people.push(new Person(random(r, windowWidth-r), random(r, windowHeight-r), r, false));
+        people.push(new Person(random(r, field.w-r), random(r, field.h-r), r, false));
     }
     people[0].isInfe = true;
 }
 
 function draw() {
     background(0);
-
+    drawStroke(field.offset, field.offset, field.w-field.offset*2, field.h-field.offset*2);
     for(let i = 0; i < people.length; i++){
         people[i].update();
-        people[i].bounce();
+        people[i].bounce(field);
         for(let j = 0; j < people.length; j++){
             if (j == i){
                 continue;
@@ -29,6 +33,16 @@ function draw() {
         }
         people[i].display();
     }
+    push();
+    translate(0, field.h);
+    drawStroke(field.offset, 0, field.w-field.offset*2, infoField.h-field.offset)
+    pop();
+}
+
+function drawStroke(x, y, w, h){
+    fill(0, 0)
+    stroke(150)
+    rect(x, y, w, h);
 }
 
 //////////////////////////////////
@@ -54,7 +68,6 @@ class Person{
         this.isInfe ? this.c = color(255, 0, 0) : this.c = color(255, 255, 255);
         this.maxspeed = 2;
         this.maxforce = 0.1;
-        this.boost = 1.5;
     }
 
     update(){
@@ -68,12 +81,12 @@ class Person{
         }
     }
 
-    bounce(){
-        var offset = this.r * 0.5;
-        if((this.location.x >= windowWidth - offset) || (this.location.x <= offset)){
+    bounce(f){
+        var offset = this.r * 0.5 + f.offset;
+        if((this.location.x >= f.w - offset) || (this.location.x <= offset)){
           this.velocity.x *= -1;
         }
-        if((this.location.y >= windowHeight - offset) || (this.location.y <= offset)){
+        if((this.location.y >= f.h - offset) || (this.location.y <= offset)){
           this.velocity.y *= -1;
         }
     }
@@ -81,8 +94,6 @@ class Person{
     display() {
         fill(this.c);
         noStroke();
-        push();
         ellipse(this.location.x, this.location.y, this.r, this.r);
-        pop();
     }
 }
