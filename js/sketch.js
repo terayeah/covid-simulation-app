28fb958e47c3b06d9a8c6f32c59bdc9a61054625
dterai <dterai@student.42tokyo.jp>
2021-01-01 01:15:16 +0900

    重ならないようにし各々に簡易的な衝突判定を与えた

--- js/sketch.js
+++ js/sketch.js
@@ -1,66 +1,75 @@
+//////////////////////////////////
+////////////Auguments/////////////
+//////////////////////////////////
+
 let people = [];
+let locs = [];
 let r = 20;
-let maxDensity;
+let density;
+let maxDensity = 500;
 let field;
 let infoField;
 let actionButton;
 let restartButton;
 let isLoop = true;
 let isReset = false;
-let frame;
+let frame = {
+    count:0,
+    rate:30,
+    max:300
+};
+let params = {
+    InfectionRate: 100,
+    CaseFatalityRate: 100,
+    MoveRate: 100,
+    IncubationFrame: 150,
+    OnsetFrame: 300
+}
+
+//////////////////////////////////
+//////////Main Functions//////////
+//////////////////////////////////
 
 function setup() {
-    frame = {count:0, rate:30, max:300}
-    infoField = {w: windowWidth, h:200}
-    field = {w: windowWidth, h:windowHeight-infoField.h, offset:10}
-    maxDensity = field.w/(r*2) * field.h/(r*2)
+    setupData();
     createCanvas(windowWidth, windowHeight);
     frameRate(frame.rate);
-    for(var i = 0; i < maxDensity; i++){
-        people.push(new Person(random(r, field.w-r), random(r, field.h-r), r));
-    }
-    people[0].isInfe = true;
-    actionButton = createButton('pause');
-    actionButton.id('actionButton');
-    actionButton.position(field.w - 200, field.h + field.offset);
-    actionButton.mousePressed(()=>onActionButtonPressed());
-    restartButton = createButton('restart');
-    restartButton.id('restartButton');
-    restartButton.position(field.w - 200, field.h + field.offset * 5);
-    restartButton.mousePressed(()=>onRestartButtonPressed(isReset));
+    initPeople();
+    setButtons(actionButton, restartButton);
 }
 
 function draw() {
     background(0);
     drawStroke(field.offset, field.offset, field.w-field.offset*2, field.h-field.offset*2);
+    push();
+    translate(0, field.h);
+    drawStroke(field.offset, 0, field.w-field.offset*2, infoField.h-field.offset)
+    pop();
+
     if (isReset){
-        people = [];
-        for(var i = 0; i < maxDensity; i++){
-            people.push(new Person(random(r, field.w-r), random(r, field.h-r), r));
-        }
-        people[0].isInfe = true;
+        initPeople();
         frame.count = 0;
         isReset = !isReset;
     }
     for(let i = 0; i < people.length; i++){
         people[i].update();
-        people[i].bounce(field);
         for(let j = 0; j < people.length; j++){
             if (j == i){
                 continue;
             }
             let loc = people[j].location.copy();
             let distance = loc.sub(people[i].location).mag();
-            if (distance < people[i].r && !people[i].isInfe && people[j].isInfe){
-                people[i].isInfe = true;
+            if (distance <= people[i].r){
+                people[i].velocity.mult(-1);
+                if (!people[i].status.isInfection && people[j].status.isInfection && getRate(people[i].params.InfectionRate)){
+                    people[i].status.isInfection = true;
+                }
+                break;
             }
         }
+        people[i].bounce(field);
         people[i].display();
     }
-    push();
-    translate(0, field.h);
-    drawStroke(field.offset, 0, field.w-field.offset*2, infoField.h-field.offset)
-    pop();
     if (frame.count > frame.max){
         isReset = true;
         frame.count = 0;
@@ -70,10 +79,63 @@ function draw() {
     frame.count++;
 }
 
+//////////////////////////////////
+//////////Util Functions//////////
+//////////////////////////////////
+
+function setupData(){
+    infoField = {w: windowWidth, h:200}
+    field = {w: windowWidth, h:windowHeight-infoField.h, offset:10}
+    density = 100
+}
+
+function initPeople(){
+    // 重ならないようにrandomに配置する
+    people = [];
+    locs = [];
+    let isOk = true;
+    for(let i = 0; i < density; i++){
+        let loc = createVector(random(field.offset + r, field.w - r), random(field.offset + r, field.h - r));
+        for (let j = 0; j < locs.length; j++){
+            let tmp = loc.copy();
+            if (tmp.sub(locs[j]).mag() <= r*1.5){
+                isOk = false;
+                break;
+            }
+        }
+        if (isOk){
+            locs.push(loc);
+        }else{
+            isOk = true;
+            i--;
+        }
+    }
+    for(let i = 0; i < locs.length; i++){
+        people.push(new Person(locs[i].x, locs[i].y, r));
+    }
+    people[0].status.isInfection = true;
+}
+
 function drawStroke(x, y, w, h){
     fill(0, 0)
     stroke(150)
     rect(x, y, w, h);
+}
+
+function getRate(percentage){
+    let p = Math.random() * 100;
+    return (p < percentage);
+}
+
+function setButtons(actionButton, restartButton){
+    actionButton = createButton('pause');
+    actionButton.id('actionButton');
+    actionButton.position(field.w - 200, field.h + field.offset);
+    actionButton.mousePressed(()=>onActionButtonPressed());
+    restartButton = createButton('restart');
+    restartButton.id('restartButton');
+    restartButton.position(field.w - 200, field.h + field.offset * 5);
+    restartButton.mousePressed(()=>onRestartButtonPressed(isReset));
 }
 
 function onActionButtonPressed(){
@@ -102,19 +164,33 @@ class Person{
         let velocity;
         let acceleration;
         let r;
-        let isInfe;
+        let status;
         let c;
         let maxspeed;
         let maxforce;
+        let params;
 
         this.acceleration = createVector(random(-1, 1), random(-1, 1));
         this.velocity = createVector(0, 0);
         this.location = createVector(x, y);
         this.r = radius;
-        this.isInfe = false;
+        this.status = {
+            isInfection: false
+        }
         this.c = color(255, 255, 255);
         this.maxspeed = 2;
         this.maxforce = 0.1;
+        this.params = {
+            InfectionRate: 0,
+            CaseFatalityRate: 100,
+            MoveRate: 100,
+            IncubationFrame: 150,
+            OnsetFrame: 300
+        };
+    }
+
+    setParams(params){
+        this.params = p;
     }
 
     update(){
@@ -123,13 +199,13 @@ class Person{
         this.velocity.limit(this.maxspeed);
         this.location.add(this.velocity);
         this.acceleration.mult(0);
-        if (this.isInfe){
+        if (this.status.isInfection){
             this.c = color(255, 0, 0);
         }
     }
 
     bounce(f){
-        var offset = this.r * 0.5 + f.offset;
+        var offset = f.offset + this.r * 0.5;
         if((this.location.x >= f.w - offset) || (this.location.x <= offset)){
           this.velocity.x *= -1;
         }
