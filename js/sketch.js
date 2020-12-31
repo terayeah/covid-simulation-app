923238e84418abc8e6db1c99b1d3e471c0cc3863
dterai <dterai@student.42tokyo.jp>
2020-12-31 16:32:23 +0900

    時間とボタンの作成

--- js/sketch.js
+++ js/sketch.js
@@ -3,21 +3,45 @@ let r = 20;
 let maxDensity;
 let field;
 let infoField;
+let actionButton;
+let restartButton;
+let isLoop = true;
+let isReset = false;
+let frame;
 
 function setup() {
+    frame = {count:0, rate:30, max:300}
     infoField = {w: windowWidth, h:200}
     field = {w: windowWidth, h:windowHeight-infoField.h, offset:10}
     maxDensity = field.w/(r*2) * field.h/(r*2)
     createCanvas(windowWidth, windowHeight);
+    frameRate(frame.rate);
     for(var i = 0; i < maxDensity; i++){
-        people.push(new Person(random(r, field.w-r), random(r, field.h-r), r, false));
+        people.push(new Person(random(r, field.w-r), random(r, field.h-r), r));
     }
     people[0].isInfe = true;
+    actionButton = createButton('pause');
+    actionButton.id('actionButton');
+    actionButton.position(field.w - 200, field.h + field.offset);
+    actionButton.mousePressed(()=>onActionButtonPressed());
+    restartButton = createButton('restart');
+    restartButton.id('restartButton');
+    restartButton.position(field.w - 200, field.h + field.offset * 5);
+    restartButton.mousePressed(()=>onRestartButtonPressed(isReset));
 }
 
 function draw() {
     background(0);
     drawStroke(field.offset, field.offset, field.w-field.offset*2, field.h-field.offset*2);
+    if (isReset){
+        people = [];
+        for(var i = 0; i < maxDensity; i++){
+            people.push(new Person(random(r, field.w-r), random(r, field.h-r), r));
+        }
+        people[0].isInfe = true;
+        frame.count = 0;
+        isReset = !isReset;
+    }
     for(let i = 0; i < people.length; i++){
         people[i].update();
         people[i].bounce(field);
@@ -37,6 +61,13 @@ function draw() {
     translate(0, field.h);
     drawStroke(field.offset, 0, field.w-field.offset*2, infoField.h-field.offset)
     pop();
+    if (frame.count > frame.max){
+        isReset = true;
+        frame.count = 0;
+        noLoop();
+        isLoop = false;
+    }
+    frame.count++;
 }
 
 function drawStroke(x, y, w, h){
@@ -45,12 +76,28 @@ function drawStroke(x, y, w, h){
     rect(x, y, w, h);
 }
 
+function onActionButtonPressed(){
+    if (isReset)return;
+    isLoop = !isLoop;
+    isLoop ? loop() : noLoop();
+    let b = document.querySelector('#actionButton');
+    let rb = document.querySelector('#restartButton');
+    isLoop ? b.innerText = "pause": b.innerText="start"
+    isLoop ? rb.style.display = "block": rb.style.display="none"
+}
+
+function onRestartButtonPressed(){
+    isLoop = true;
+    loop();
+    isReset = !isReset;
+}
+
 //////////////////////////////////
 ///////////Person Class///////////
 //////////////////////////////////
 
 class Person{
-    constructor(x, y, radius, isInfected){
+    constructor(x, y, radius){
         let location;
         let velocity;
         let acceleration;
@@ -64,8 +111,8 @@ class Person{
         this.velocity = createVector(0, 0);
         this.location = createVector(x, y);
         this.r = radius;
-        this.isInfe = isInfected;
-        this.isInfe ? this.c = color(255, 0, 0) : this.c = color(255, 255, 255);
+        this.isInfe = false;
+        this.c = color(255, 255, 255);
         this.maxspeed = 2;
         this.maxforce = 0.1;
     }
