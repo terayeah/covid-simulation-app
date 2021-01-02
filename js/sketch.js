7d958e103d74c70576dede294be856276fcaaa87
dterai <harrypoppopoo@gmail.com>
2021-01-02 19:00:07 +0900

    paramに対応

--- js/sketch.js
+++ js/sketch.js
@@ -18,11 +18,11 @@ let isReset = false;
 let frame = {
     count:0,
     rate:30,
-    max:300
+    max:1000
 };
 let params = {
     InfectionRate: 50, //感染率
-    CaseFatalityRate: 100, //致死率
+    CaseFatalityRate: 50, //致死率
     MoveRate: 100, //移動率
     IncubationFrame: 150, //潜伏期間
     OnsetFrame: 300 //発症期間
@@ -44,6 +44,7 @@ function setup() {
 
 function draw() {
     background(0);
+    strokeWeight(3);
     drawStroke(field.offset, field.offset, field.w-field.offset*2, field.h-field.offset*2);
     push();
     translate(0, field.h);
@@ -57,31 +58,29 @@ function draw() {
     }
     for(let i = 0; i < people.length; i++){
         people[i].update();
+        people[i].updateFlag(params.IncubationFrame, params.OnsetFrame);
         for(let j = 0; j < people.length; j++){
             if (j == i){
                 continue;
             }
             let loc = people[j].location.copy();
             let distance = loc.sub(people[i].location).mag();
-            if (distance <= people[i].r){
+            if (distance <= people[i].r * 1.1){
                 people[i].velocity.mult(-1);
-                if (!people[i].status.isInfection && people[j].status.isInfection
-                    && getRate(people[i].params.InfectionRate)){
-                    people[i].status.isInfection = true;
-                }
+                people[i].contact(people[j].flag.isInfection);
                 break;
             }
         }
         people[i].bounce(field);
         people[i].display();
     }
+    frame.count++;
     if (frame.count > frame.max){
         isReset = true;
         frame.count = 0;
         noLoop();
         isLoop = false;
     }
-    frame.count++;
 }
 
 ////////////////////////////////////////////////////////////////////////////////////
@@ -102,7 +101,7 @@ function initPeople(){
     let isOk = true;
     for(let i = 0; i < density; i++){
         let loc = createVector(
-            random(field.offset + r, field.w - r),random(field.offset + r, field.h - r)
+            random(field.offset + r*2, field.w - r*2),random(field.offset + r*2, field.h - r*2)
         );
         for (let j = 0; j < locs.length; j++){
             let tmp = loc.copy();
@@ -121,7 +120,7 @@ function initPeople(){
     for(let i = 0; i < locs.length; i++){
         people.push(new Person(locs[i].x, locs[i].y, r));
     }
-    people[0].status.isInfection = true;
+    people[0].infect(params.IncubationFrame);
     for(let i = 0; i < people.length; i++){
         people[i].setParams(params);
     }
@@ -131,11 +130,6 @@ function drawStroke(x, y, w, h){
     fill(0, 0)
     stroke(150)
     rect(x, y, w, h);
-}
-
-function getRate(percentage){
-    let p = Math.random() * 100;
-    return (p < percentage);
 }
 
 function setButtons(actionButton, restartButton){
@@ -177,19 +171,27 @@ class Person{
         let velocity;
         let acceleration;
         let r;
-        let status;
+        let colors;
         let c;
+        let sc;
         let maxspeed;
         let params;
+        let f;
+        let flag;
 
         this.acceleration = createVector(random(-1, 1), random(-1, 1));
         this.velocity = createVector(0, 0);
         this.location = createVector(x, y);
         this.r = radius;
-        this.status = {
-            isInfection: false
+        this.colors = {
+            normal: color(255, 255, 255),
+            incubation: color(255, 0, 0),
+            infect: color(255, 0, 0),
+            anti: color(0, 0, 255),
+            dead: color(100, 100, 100)
         }
-        this.c = color(255, 255, 255);
+        this.c = this.colors.normal;
+        this.sc = this.colors.normal;
         this.maxspeed = 2;
         this.params = {
             InfectionRate: 100,
@@ -198,25 +200,76 @@ class Person{
             IncubationFrame: 150,
             OnsetFrame: 300
         };
+        this.f = 0;
+        this.flag = {
+            isInfection: false,
+            isDead: false,
+            isAntiBody: false
+        }
     }
 
     setParams(p){
         this.params = p;
     }
 
+    setColor(c, sc){
+        this.c = c;
+        this.sc = sc;
+    }
+
+    contact(other){
+        if (!this.flag.isAntiBody && !this.flag.isInfection && other && this.getRate(this.params.InfectionRate)){
+            this.flag.isInfection = true;
+        }
+    }
+
+    getRate(percentage){
+        let p = Math.random() * 100;
+        return (p < percentage);
+    }
+
+    infect(incuEnd){
+        this.flag.isInfection = true;
+        this.f = incuEnd;
+    }
+
     update(){
+        if (this.flag.isDead){
+            this.velocity.mult(0);
+            return;
+        }
         this.velocity.normalize();
         this.velocity.add(this.acceleration).mult(2);
         this.velocity.limit(this.maxspeed);
         this.location.add(this.velocity);
         this.acceleration.mult(0);
-        if (this.status.isInfection){
-            this.c = color(255, 0, 0);
+    }
+
+    updateFlag(incuEnd, deadLine){
+        if (this.flag.isDead || !this.flag.isInfection) return;
+        this.f++;
+        // 潜伏
+        this.setColor(this.colors.normal, this.colors.incubation)
+        if (this.f > incuEnd){
+            // 感染
+            this.setColor(this.colors.infect, this.colors.infect)
+        }
+        if (this.f > deadLine){
+            if (this.getRate(this.params.CaseFatalityRate)){
+                // 死
+                this.setColor(this.colors.dead, this.colors.dead)
+                this.flag.isDead = true;
+            }else{
+                // 復活
+                this.setColor(this.colors.normal, this.colors.anti)
+                this.flag.isAntiBody = true;
+            }
+            this.flag.isInfection = false;
         }
     }
 
     bounce(f){
-        var offset = f.offset + this.r * 0.5;
+        var offset = (f.offset + this.r * 0.5) * 1.1;
         if((this.location.x >= f.w - offset) || (this.location.x <= offset)){
           this.velocity.x *= -1;
         }
@@ -227,7 +280,7 @@ class Person{
 
     display() {
         fill(this.c);
-        noStroke();
+        stroke(this.sc);
         ellipse(this.location.x, this.location.y, this.r, this.r);
     }
 }
